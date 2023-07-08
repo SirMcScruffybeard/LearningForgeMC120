cbbb00c3cee0e116fd81df2d605da608a23d244d
SirMcScruffybeard <scruffybeardworx@gmail.com>
2023-07-08 18:37:57 -0400

    New Items Added Alexandrite and Raw Alexandrite items. Added file structure for items Corrections

--- src/main/java/net/mrscruffybeard/mccourse/MCCourseMod.java
+++ src/main/java/net/mrscruffybeard/mccourse/MCCourseMod.java
@@ -1,6 +1,8 @@
 package net.mrscruffybeard.mccourse;
 
 import com.mojang.logging.LogUtils;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
@@ -13,6 +15,7 @@ import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.mrscruffybeard.mccourse.item.ModItems;
 import org.slf4j.Logger;
 
 // The value here should match an entry in the META-INF/mods.toml file
@@ -20,13 +23,15 @@ import org.slf4j.Logger;
 public class MCCourseMod {
 
     // Define mod id in a common place for everything to reference
-    public static final String MOD_ID = "mccorse";
+    public static final String MOD_ID = "mccourse";
     // Directly reference a slf4j logger
     private static final Logger LOGGER = LogUtils.getLogger();
 
     public MCCourseMod() {
 
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        ModItems.register(modEventBus);
 
         modEventBus.addListener(this::commonSetup);
 
@@ -41,6 +46,10 @@ public class MCCourseMod {
 
     private void addCreative(BuildCreativeModeTabContentsEvent event) {
 
+        if (event.getTabKey() == CreativeModeTabs.INGREDIENTS) {
+            event.accept(ModItems.ALEXANDRITE);
+            event.accept(ModItems.RAW_ALEXANDRITE);
+        }
     }
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
